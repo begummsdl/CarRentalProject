7a5200b99c813ea51947a688165f7bc6a35df638
begüm muşdal <begum_musdal@hotmail.com>
2021-03-10 02:48:51 +0300

    .

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,81 @@
+﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        [ValidationAspect(typeof(CarImageValidation))]
+        public IResult Add(CarImage carImage)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarImagesLimitExceded(carImage.CarId));
+            if (result!=null)
+            {
+                return result;
+            }
+            carImage.Date = DateTime.Now;
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> GetCarImagesByCarId(int carId)
+        {
+            IResult result = BusinessRules.Run(CheckIfImagePathOfCarImageExists(carId));
+            if (result!=null)
+            {
+                return new ErrorDataResult<List<CarImage>>(result.Message);
+            }
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(i=>i.CarId==carId));
+        }
+
+        [ValidationAspect(typeof(CarImageValidation))]
+        public IResult Update(CarImage carImage)
+        {
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCarImagesLimitExceded(int id)
+        {
+            var result = _carImageDal.GetAll(i => i.CarId == id).Count;
+            if (result>=5)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfImagePathOfCarImageExists(int id)
+        {
+            string imagePath = "Logo";
+            var result = _carImageDal.GetAll(i => i.CarId == id).Any();
+            if (!result)
+            {
+                return new ErrorResult(imagePath);
+            }
+            return new SuccessResult();
+        }
+    }
+}
